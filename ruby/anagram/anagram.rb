27b891da44079699ec620fa0240e30621eef1ec3
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-15 13:19:22 -0500

    Follow Ruby style guide

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,29 +1,29 @@
 module Stringy
-    refine String do
-        def normalize
-            downcase.chars.sort
-        end
+  refine String do
+    def normalize
+        downcase.chars.sort
     end
+  end
 end
 
 class Anagram
-    using Stringy
+  using Stringy
 
-    def initialize(word)
-        @word = word
-    end
+  def initialize(word)
+    @word = word
+  end
 
-    def match(candidates)
-        candidates.select { |candidate| same_chars?(candidate) && !same_word?(candidate) }
-    end
+  def match(candidates)
+    candidates.select { |candidate| same_chars?(candidate) && !same_word?(candidate) }
+  end
 
-    private
+  private
 
-    def same_chars?(candidate)
-        @word.normalize == candidate.normalize 
-    end
+  def same_chars?(candidate)
+    @word.normalize == candidate.normalize 
+  end
 
-    def same_word?(candidate)
-        @word.casecmp(candidate) == 0
-    end
+  def same_word?(candidate)
+    @word.casecmp(candidate) == 0
+  end
 end
