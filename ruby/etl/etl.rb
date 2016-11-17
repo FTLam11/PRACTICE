58fa9ab6fecb1b5ec60a204e0b6d21e122eb9c8b
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-16 23:25:43 -0600

    Finish solution

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,11 +1,23 @@
 class ETL
   def self.transform(legacy_data)
+    transformed = Hash.new
 
-    legacy_data.map do | points, chars |
-      points, chars = chars, points
+    legacy_data.values.each do |value|
+      value.is_a?(Array) ? 
+      swap_each_array_value_with_key(legacy_data, value, transformed) : 
+      swap_single_value_with_key(legacy_data, value, transformed)
     end
+    
+    transformed
+  end
 
-    legacy_data
+  private
 
+  def self.swap_each_array_value_with_key(legacy_data, array, transformed)
+    array.each { |e| transformed[e.downcase] = legacy_data.key(array) }
+  end
+
+  def self.swap_single_value_with_key(legacy_data, value, transformed)
+    transformed[value.downcase] = legacy_data.key([value])
   end
 end
