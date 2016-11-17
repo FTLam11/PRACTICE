9fdcd08ee6ccc4fd5c66556dbea3f7b54baafd14
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-16 22:38:42 -0600

    Use attr_reader for collection

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,14 +1,16 @@
 class Squares
+  attr_reader :collection
+
   def initialize(natural_num)
     @collection = (0..natural_num).to_a
   end
 
   def square_of_sum
-    @collection.inject(:+)**2
+    collection.inject(:+)**2
   end
 
   def sum_of_squares
-    @collection.inject { |sum, element| sum += element**2 }
+    collection.inject { |sum, element| sum += element**2 }
   end
 
   def difference
