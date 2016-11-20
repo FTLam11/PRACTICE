f0652ea3354ca7542ff5e16a76650b462d3c6a39
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 16:06:56 -0600

    Refactor prime_factors

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,15 +1,15 @@
 class PrimeFactors
   def self.for(num)
     prime_factors = Array.new
-    test_factor = 2
+    candidate = 2
     
     while num != 1 do
-      if num % test_factor == 0
-        prime_factors << test_factor
-        num /= test_factor
-        test_factor = 2
+      if num % candidate == 0
+        prime_factors << candidate
+        num /= candidate
+        candidate = 2
       else
-        test_factor += 1
+        candidate += 1
       end
     end
 
