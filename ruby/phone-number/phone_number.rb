a5359a652fbfbafe89f4c1bf5e684519f547aec4
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 16:03:32 -0600

    Refactor phone_number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,27 +1,40 @@
 class PhoneNumber
-	def initialize(num_str)
-		@num_str = num_str
-	end
+  attr_reader :phone_num, :formatted_num
 
-	def number
-		return '0000000000' if @num_str.scan(/[A-Za-z]/) != []
+  def initialize(num_str)
+    @phone_num = num_str
+    @formatted_num = num_str.scan(/\d/).join
+  end
 
-		formatted_num = @num_str.scan(/\d/).join
+  def number
+    if has_area_code_and_leading_one
+      return formatted_num[1..-1]
+    elsif invalid_length || contains_letters
+      return '0000000000'
+    else
+      return formatted_num
+    end
+  end
 
-		if formatted_num.length == 11 && formatted_num[0] == '1'
-			return formatted_num[1..-1]
-		elsif formatted_num.length > 11 || formatted_num.size != 10
-			return '0000000000'
-		else
-			return formatted_num
-		end
-	end
+  def area_code
+    self.number[0..2]
+  end
 
-	def area_code
-		self.number[0..2]
-	end
+  def to_s
+    "(#{self.number[0..2]}) #{self.number[3..5]}-#{self.number[6..-1]}"
+  end
 
-	def to_s
-		"(#{self.number[0..2]}) #{self.number[3..5]}-#{self.number[6..-1]}"
-	end
+  private
+
+  def contains_letters
+    return true if !phone_num.scan(/[A-Za-z]/).empty?
+  end
+
+  def has_area_code_and_leading_one
+    formatted_num.length == 11 && formatted_num[0] == '1'
+  end
+
+  def invalid_length
+    formatted_num.size != 10
+  end
 end
