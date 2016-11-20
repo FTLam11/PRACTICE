c03a0e2ce98eed6c2c93622ed1e14f7939d1c8dc
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 16:21:19 -0600

    Fix spacing

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,36 +1,36 @@
 class Raindrops
-	def self.convert(num)
-		output = ""
-		prime_factors(num, result = []).uniq.each do |factor|
-			case factor
-				when 3
-					output.concat('Pling')
-				when 5
-					output.concat('Plang')
-				when 7
-					output.concat('Plong')
-			end
-		end
-		output = output + "#{num}" if output == ""
-		output
-	end
+  def self.convert(num)
+    output = ""
+    prime_factors(num, result = []).uniq.each do |factor|
+      case factor
+        when 3
+          output.concat('Pling')
+        when 5
+          output.concat('Plang')
+        when 7
+          output.concat('Plong')
+      end
+    end
+    output = output + "#{num}" if output == ""
+    output
+  end
 
-	def self.prime_factors(num, result = [])
-		counter = 2
-		return result if num == 1
-		while counter <= num do
-			if num % counter == 0
-				result << counter
-				quotient = num / counter
-				break
-			else
-				counter += 1
-			end
-		end
-		prime_factors(quotient, result)
-	end
+  def self.prime_factors(num, result = [])
+    counter = 2
+    return result if num == 1
+    while counter <= num do
+      if num % counter == 0
+        result << counter
+        quotient = num / counter
+        break
+      else
+        counter += 1
+      end
+    end
+    prime_factors(quotient, result)
+  end
 end
 
 module BookKeeping
-	VERSION = 2
+  VERSION = 2
 end
