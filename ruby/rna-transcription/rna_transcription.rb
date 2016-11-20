ccb79db23accfb61e2ea180cd15444bbcd1ac1f7
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 16:24:47 -0600

    Refactor rna_transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,24 +1,18 @@
 class Complement
-	COMPLEMENT = {
-		G: 'C',
-		C: 'G',
-		T: 'A',
-		A: 'U' 
-	}
-	def self.of_dna(nucleotide)
-		# complement = ""
-		# nucleotide.chars.each do |char|
-		# complement.concat(COMPLEMENT[char.to_sym])
-		# end
-		# complement
-		complement = nucleotide.chars.map do |char|
-		char = (COMPLEMENT[char.to_sym])
-		end
-		return '' if complement.include? nil
-		complement.join
-	end
+  COMPLEMENT = {
+    G: 'C',
+    C: 'G',
+    T: 'A',
+    A: 'U' 
+  }
+
+  def self.of_dna(nucleotide)
+    complement = nucleotide.chars.map { |char| COMPLEMENT[char.to_sym] }
+    return '' if complement.include? nil
+    complement.join
+  end
 end
 
 module BookKeeping
-	VERSION = 4
+  VERSION = 4
 end
