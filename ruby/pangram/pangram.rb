c27397a5963a61bce744130fbdbc3dd5164a76bd
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 15:44:28 -0600

    Refactor pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,6 +1,6 @@
 class Pangram
-	def self.is_pangram?(str)
-		return true if str.downcase.gsub(/[\s\d_\."]/, "").split("").uniq.join("").length == 26
-		false
-	end
+  def self.is_pangram?(str)
+    return true if str.downcase.gsub(/[\s\d_\."]/, "").split("").uniq.join.length == 26
+    false
+  end
 end
