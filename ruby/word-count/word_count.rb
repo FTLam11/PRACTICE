7a076d9769f7689c65c2fd27007c2a6a69af60c4
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 21:51:30 -0600

    Refactor word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,21 +1,33 @@
 class Phrase
-	def initialize(phrase)
-		@words = phrase.downcase.gsub(/\s'{1}/, " ").gsub(/'{1}\s/," ").scan(/\w+'*\w?/)
-	end
+  attr_reader :words
 
-	def word_count
-		word_count = Hash.new(0)
-		@words.each do |word|
-			if word_count[word]
-				word_count[word] += 1
-			else
-				word_count[word] = 1
-			end
-		end
-		word_count
-	end
+  def initialize(phrase)
+    @words = phrase
+  end
+
+  def word_count
+    Hash.new(0).tap do |word_hash|
+      clean_phrase.each do |word|
+        if word_hash[word]
+          word_hash[word] += 1
+        else
+          word_hash[word] = 1
+        end
+      end
+    end
+  end
+
+  private
+
+  def clean_phrase
+    handle_quotations.scan(/\w+'*\w?/)
+  end
+
+  def handle_quotations
+    words.downcase.gsub(/\s'{1}/, " ").gsub(/'{1}\s/," ")
+  end
 end
 
 module BookKeeping
-	VERSION = 1
+  VERSION = 1
 end
