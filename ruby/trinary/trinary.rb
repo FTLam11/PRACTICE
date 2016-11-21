2cb741f7868bf6b2e61a81e1df81b757ace7006e
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 21:12:51 -0600

    Refactor trinary

--- ruby/trinary/trinary.rb
+++ ruby/trinary/trinary.rb
@@ -1,26 +1,26 @@
 class Trinary
-    def initialize(trinary_string)
-        @trinary_string = trinary_string
-    end
+  attr_reader :trinary_string
 
-    def to_decimal
-        return 0 if contains_invalid_char? || contains_invalid_num?
+  def initialize(trinary_string)
+    @trinary_string = trinary_string
+  end
 
-        @trinary_string.chars.map(&:to_i).reverse.map.with_index { | num, index |
-            num = num.to_i * 3**index }.inject(0, :+)
-    end
+  def to_decimal
+    return 0 if contains_invalid_char? || contains_invalid_num?
+    trinary_string.chars.map(&:to_i).reverse.map.with_index { | num, index | num * 3**index }.inject(&:+)
+  end
 
-    private
+  private
 
-    def contains_invalid_char?
-        return true if @trinary_string.chars.grep(/\D/) != []
-    end
+  def contains_invalid_char?
+    trinary_string.chars.grep(/\D/) != []
+  end
 
-    def contains_invalid_num?
-        return true if @trinary_string.chars.any? { | char | char.to_i > 2 }
-    end
+  def contains_invalid_num?
+    trinary_string.chars.any? { | char | char.to_i > 2 }
+  end
 end
 
 module BookKeeping
-    VERSION = 1
+  VERSION = 1
 end
