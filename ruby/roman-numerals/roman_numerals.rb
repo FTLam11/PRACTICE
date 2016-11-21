b8f0e067d5cd67d43530b6f0a5060542d95d1af6
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 19:38:04 -0600

    Refactor roman numerals

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,50 +1,47 @@
 module ArabicToRoman
-	CONVERT = {
-		1000 => "M",
-		500 => "D",
-		100 => "C",
-		50 => "L",
-		10 => "X",
-		5 => "V",
-		1 => "I"
-	}
+  CONVERT = {
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    5 => "V",
+    1 => "I"
+  }
 
-	REPLACE = {
-		"DCCCC" => "CM",
-		"CCCC" => "CD",
-		"LXXXX" => "XC",
-		"XXXX" => "XL",
-		"VIIII" => "IX",
-		"IIII" => "IV"
-	}
+  REPLACE = {
+    "DCCCC" => "CM",
+    "CCCC" => "CD",
+    "LXXXX" => "XC",
+    "XXXX" => "XL",
+    "VIIII" => "IX",
+    "IIII" => "IV"
+  }
 end
 
 class Fixnum
-	include ArabicToRoman
+  include ArabicToRoman
 
-	def to_roman
-		classic_roman = ""
-		num = self
+  def to_roman
+    num = self
 
-		CONVERT.each do | arbc, rmn |
-			(num / arbc).times { classic_roman.concat(rmn) }
-			num = num % arbc
-		end
+    String.new.tap do |string|
+      CONVERT.each do | arbc, rmn |
+        (num / arbc).times { string.concat(rmn) }
+        num %= arbc
+      end
+    end
+  end
 
-		classic_roman
-	end
-
-	def arabic_to_modern_roman
-		modern_roman_num =	self.to_roman
-
-		REPLACE.each do | key, value |
-			modern_roman_num.sub!(key, value) if modern_roman_num.include? key
-		end
-
-		modern_roman_num
-	end
+  def arabic_to_modern_roman
+    self.to_roman.tap do |modern_roman_num|
+      REPLACE.each do | key, value |
+        modern_roman_num.sub!(key, value) if modern_roman_num.include? key
+      end
+    end
+  end
 end
  
 module BookKeeping
-	VERSION = 2
+  VERSION = 2
 end
