cbe46706d2d07f919ee1bc44270137046fbdf707
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-20 20:32:43 -0600

    Refactor sieve

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,21 +1,28 @@
 class Sieve
-	def initialize(n)
-		@collection = Hash.new(0)
-		(2..n).each do |num|
-			@collection[num] = 0
-		end
-	end
+  def initialize(n)
+    @collection = populate_collection(n)
+  end
 
-	def primes
-		@collection.each do | k, v |
-			if v == 0
-				@collection.each do | key, value |
-					next if key == k
-					@collection[key] = 1 if key % k == 0
-				end
-			end
-		end
+  def primes
+    mark_collection.select { |key, value| value == 0 }.keys
+  end
 
-		@collection.select { |key, value| value == 0 }.keys
-	end
+  private
+
+  def populate_collection(n)
+    Hash.new(0).tap do |collection|
+      (2..n).each { |num| collection[num] = 0 }
+    end
+  end
+
+  def mark_collection
+    @collection.each do | k, v |
+      if v == 0
+        @collection.each do | key, value |
+          next if key == k
+          @collection[key] = 1 if key % k == 0
+        end
+      end
+    end
+  end
 end
