ceaed1a6189430117e8134f71864699594ec79a4
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-25 13:36:32 -0600

    Move hex decimal map from initialize to class constant

--- ruby/hexadecimal/hexadecimal.rb
+++ ruby/hexadecimal/hexadecimal.rb
@@ -1,21 +1,18 @@
 class Hexadecimal
   attr_reader :hex, :hex_decimal_map
 
+  HEX_DECIMAL_MAP = ('a'..'f').to_a.zip((10..15).to_a).to_h
+
   def initialize(str)
     @hex = str
-    @hex_decimal_map = create_hex_decimal_map
   end
 
   def to_decimal
     return 0 if invalid_hex?(hex)
-    hex.chars.reverse.map.with_index { |char, i| hex_decimal_map.fetch(char, char).to_i * 16**i }.inject(&:+)
+    hex.chars.reverse.map.with_index { |char, i| HEX_DECIMAL_MAP.fetch(char, char).to_i * 16**i }.inject(&:+)
   end
 
   private
-
-  def create_hex_decimal_map
-    ('a'..'f').to_a.zip((10..15).to_a).to_h
-  end
 
   def invalid_hex?(str)
     not str.scan(/[^\da-f]/).empty?
