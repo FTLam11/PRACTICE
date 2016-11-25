27935accddae1c8bcf7bb506a291749dd0eeff27
frosted_fronks <ryzingsun11@yahoo.com>
2016-11-25 13:29:24 -0600

    Pass sixth test

---
+++ ruby/hexadecimal/hexadecimal.rb
@@ -0,0 +1,18 @@
+class Hexadecimal
+  attr_reader :hex, :hex_decimal_map
+
+  def initialize(str)
+    @hex = str
+    @hex_decimal_map = create_hex_decimal_map
+  end
+
+  def to_decimal
+    hex.chars.reverse.map.with_index { |char, i| hex_decimal_map.fetch(char, char).to_i * 16**i }.inject(&:+)
+  end
+
+  private
+
+  def create_hex_decimal_map
+    ('a'..'f').to_a.zip((10..15).to_a).to_h
+  end
+end
