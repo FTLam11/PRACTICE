577591ea0b05657841e733790f0837312ca57b32
frosted_fronks <ryzingsun11@yahoo.com>
2016-12-15 13:04:47 -0800

    Clean up code

--- ruby/bracket-push/bracket_push.rb
+++ ruby/bracket-push/bracket_push.rb
@@ -1,7 +1,4 @@
 class Brackets
-  # filter only parens, brackets, braces
-  # compare start and end characters
-  # continue comparing if chars are equal
   def self.paired?(expression)
     legal?(clean(expression))
   end
@@ -18,21 +15,6 @@ class Brackets
     expression.scan(/[(){}\[\]]/)
   end
 
-  # def self.equal_ends?(string)
-  #   return false unless PAIR.fetch(string.first, nil)
-  #   true if string.last == PAIR[string.first]
-  # end
-
-  # def self.legal?(string)
-  #   return true if string.empty?
-  #   if equal_ends?(string)
-  #     string.shift
-  #     string.pop
-  #   else
-  #     return false
-  #   end
-  #   legal?(string)
-  # end
   def self.legal?(expression, parsed = Array.new)
     expression.each do |char|
       if open?(char)
@@ -49,7 +31,6 @@ class Brackets
   def self.open?(char)
     char.match(/[(\[{]/) != nil
   end
-
 end
 
 module BookKeeping
