13ce0811b7e2728022bb7296e531fa7a9f77dc5e
frosted_fronks <ryzingsun11@yahoo.com>
2017-01-19 09:53:42 -0800

    Pass all tests

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,7 +3,8 @@ class Year {
     this.year = year;
   }
   isLeap() {
-    if (this.year % 4 == 0 && this.year % 100 != 0) {
+    if (this.year % 4 == 0 && 
+      (this.year % 100 != 0 || this.year % 400 == 0)) {
       return true;
     } else {
       return false;
