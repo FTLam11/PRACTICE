0ba3524ea39901fcea91ff648aa1cc3ce88fb360
frosted_fronks <ryzingsun11@yahoo.com>
2017-01-19 10:28:02 -0800

    Pass all tests, string.size is not a method

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,6 +1,13 @@
 class Hamming {
   constructor() {}
+  unequalStrandLength(strandA, strandB) {
+    return strandA.length != strandB.length;
+  }
   compute(strandA, strandB) {
+    if (this.unequalStrandLength(strandA, strandB)) {
+      throw 'DNA strands must be of equal length.';
+    }
+    
     let delta = 0;
     for (var i = 0; i < strandA.length; i++) {
       if (strandA[i] != strandB[i]) {
