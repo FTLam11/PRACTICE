373054a1875d98b0b5f5fa736b12e95cfbb171a7
frosted_fronks <ryzingsun11@yahoo.com>
2017-01-28 15:14:29 -0800

    Pass third test

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,12 +3,17 @@ class Bob {
   hey(phrase) {
     if (this.shouting(phrase)) {
       return 'Whoa, chill out!';
+    } else if (this.asking(phrase)) {
+      return 'Sure.';
     } else {
       return 'Whatever.';
-    }
+    };
   }
   shouting(phrase) {
     return phrase === phrase.toUpperCase();
+  }
+  asking(phrase) {
+    return phrase[phrase.length - 1] === '?';
   }
 }
 
