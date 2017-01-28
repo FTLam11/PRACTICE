74e4b0c5718de72c9fccf0d2911ea39ea60af638
frosted_fronks <ryzingsun11@yahoo.com>
2017-01-28 15:29:48 -0800

    Pass all tests

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,6 +5,8 @@ class Bob {
       return 'Whoa, chill out!';
     } else if (this.asking(phrase)) {
       return 'Sure.';
+    } else if (this.silence(phrase)) {
+      return 'Fine. Be that way!';
     } else {
       return 'Whatever.';
     };
@@ -15,6 +17,9 @@ class Bob {
   }
   asking(phrase) {
     return phrase[phrase.length - 1] === '?';
+  }
+  silence(phrase) {
+    return /^\s+/.test(phrase) || phrase === '';
   }
 }
 
