c4077264483cd1ba843ea97e554f109822772383
frosted_fronks <ryzingsun11@yahoo.com>
2017-01-28 15:11:09 -0800

    Pass second test

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,7 +1,14 @@
 class Bob {
   constructor() {}
   hey(phrase) {
-    return 'Whatever.';
+    if (this.shouting(phrase)) {
+      return 'Whoa, chill out!';
+    } else {
+      return 'Whatever.';
+    }
+  }
+  shouting(phrase) {
+    return phrase === phrase.toUpperCase();
   }
 }
 
