87bab4a4eaa194644c633913fef91b9d6652b7e6
frosted_fronks <ryzingsun11@yahoo.com>
2017-03-12 01:28:35 -0800

    Try recursion

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,6 +1,15 @@
 class FlattenArray
   def self.flatten(list)
-    list.flatten.compact
+    return list.compact if unnested?(list)
+    temp = Array.new
+    list.each do |el|
+      el.is_a?(Array) ? el.each { |x| temp.push(x) } : temp.push(el)
+    end
+    self.flatten(temp)
+  end
+
+  def self.unnested?(list)
+    list.select { |e| e.is_a?(Array) }.empty?
   end
 
   VERSION = 1
