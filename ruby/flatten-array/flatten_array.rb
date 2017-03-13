0d77c2fac8ed3f645e18ff4bffb542c857bc6392
frosted_fronks <ryzingsun11@yahoo.com>
2017-03-12 21:49:50 -0700

    Refactor with tap

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,11 +1,12 @@
 class FlattenArray
   def self.flatten(list)
     return list.compact if unnested?(list)
-    temp = Array.new
-    list.each do |el|
-      el.is_a?(Array) ? el.each { |x| temp.push(x) } : temp.push(el)
+
+    next_list = Array.new.tap do |arr|
+      list.each { |el| el.is_a?(Array) ? el.each { |e| arr.push(e) } : arr.push(el) }
     end
-    self.flatten(temp)
+
+    self.flatten(next_list)
   end
 
   def self.unnested?(list)
