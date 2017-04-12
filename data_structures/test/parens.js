e03c0c2286785ab165b70db7e2c19b8952239576
frosted_fronks <ryzingsun11@yahoo.com>
2017-04-12 16:50:27 -0700

    Update test syntax

--- data_structures/test/parens.js
+++ data_structures/test/parens.js
@@ -5,14 +5,14 @@ const parens = require('../parens');
 
 describe('parens', function() {
   it('handles legal simple expressions', function() {
-    expect(parens('(2)')).to.eql(true);
+    expect(parens('(2)')).to.be.true;
   });
 
   it('handles intermediate expressions', function() {
-    expect(parens('{[(2+4)/0.5*20]^2}')).to.eql(true);
+    expect(parens('{[(2+4)/0.5*20]^2}')).to.be.true;
   });
 
   it('detects illegal expressions', function() {
-    expect(parens('Math.min(5,(6-3))(')).to.eql(false);
+    expect(parens('Math.min(5,(6-3))(')).to.be.false;
   });
 });
