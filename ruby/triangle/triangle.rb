b67afda78c5943aa49f6077f4d284b78038d5302
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-04-22 02:35:58 +0800

    Sort and use positive logic

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -2,36 +2,33 @@ class Triangle
   attr_reader :sides
 
   def initialize(sides)
-    @sides = sides
+    @sides = sides.sort
   end
 
   def equilateral?
-    return false if has_illegal_side? || violates_inequality?
-    sides.uniq.size == 1
+    valid? && sides.uniq.size == 1
   end
 
   def isosceles?
-    return false if has_illegal_side? || violates_inequality?
-    return true if equilateral?
-    sides.uniq.size == 2
+    valid? && sides.uniq.size <= 2
   end
 
   def scalene?
-    return false if has_illegal_side? || violates_inequality?
-    sides.uniq.size == 3
+    valid? && sides.uniq.size == 3
   end
 
   private
 
-  def has_illegal_side?
-    sides.any? { |side| side.zero? }
+  def valid?
+    has_nonzero_sides? && respects_triangle_inequality?
   end
 
-  def violates_inequality?
-    return true if sides[0] + sides[1] < sides[2] ||
-                   sides[2] + sides[0] < sides[1] ||
-                   sides[1] + sides[2] < sides[0]
-    false
+  def has_nonzero_sides?
+    sides[0] > 0
+  end
+
+  def respects_triangle_inequality?
+    sides[2] <= sides[0] + sides[1]
   end
 end
 
