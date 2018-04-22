4584660cc9d71401040f2a8941090ad2e48121f6
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-04-22 14:48:31 +0800

    Take 3

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,14 +1,13 @@
 class Prime
   def self.nth(num)
     raise ArgumentError if num == 0
-    primes = [2]
     candidate = 3
 
-    until primes.size == num
-      primes << candidate if (2...candidate).all? { |n| candidate % n != 0 }
-      candidate += 1
-    end
-
-    primes.last
+    [2].tap do |primes|
+      until primes.size == num
+        primes << candidate if (2...candidate).all? { |n| candidate % n != 0 }
+        candidate += 1
+      end
+    end.last
   end
 end
